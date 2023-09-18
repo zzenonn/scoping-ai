547e706fd4e395ec43afe40da074d4e3b4f5cd92
zzenonn <miguel.saavedra@obf.ateneo.edu>
2023-09-18 16:23:55 +0800

    Change answer to pointer to prevent marshalling

--- internal/message/message.go
+++ internal/message/message.go
@@ -44,7 +44,7 @@ type Message struct {
 	Id          string     `json:"id" firestore:"id"`
 	UserId      *string    `json:"user_id,omitempty" firestore:"-"`
 	MessageText *string    `json:"message_text,omitempty" firestore:"message_text,omitempty"`
-	Answer      Answer     `json:"answer,omitempty" firestore:"answer,omitempty"`
+	Answer      *Answer    `json:"answer,omitempty" firestore:"answer,omitempty"`
 	CreatedAt   *time.Time `json:"created_at,omitempty" firestore:"created_at,omitempty"`
 	UpdatedAt   *time.Time `json:"updated_at,omitempty" firestore:"updated_at,omitempty"`
 }
